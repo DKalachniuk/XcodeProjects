19bd7a5c8c825aeb14208b66af735210c5de4c0d
Kalachniuk, Dima <dkalachniuk@gmail.com>
2020-06-22 17:51:27 +0200

    initial setup of unit tests

---
+++ XcodeProjectsTests/Data/TerminalCommandTests.swift
@@ -0,0 +1,19 @@
+//
+//  TerminalScriptTests.swift
+//  XcodeProjectsTests
+//
+//  Created by Dima Kalachniuk on 22/06/2020.
+//  Copyright © 2020 com.dkcompany.xcodeprojects. All rights reserved.
+//
+
+import XCTest
+@testable import XcodeProjects
+
+class TerminalScriptTests: XCTestCase {
+
+    func testTerminalCommand_Script_ReturnsCorrectScriptValue() {
+        let terminalCommand = TerminalScript(command: "pod install")
+
+    }
+
+}
