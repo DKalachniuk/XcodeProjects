dcc4f74bb859ad93dcc3077722a8e4ca796d3ff2
Kalachniuk, Dima <dkalachniuk@gmail.com>
2020-06-22 22:23:14 +0200

    added onHover action for the cell

---
+++ XcodeProjects/UI/Modifiers/HoverModifier.swift
@@ -0,0 +1,28 @@
+//
+//  HoverModifier.swift
+//  XcodeProjects
+//
+//  Created by Dima Kalachniuk on 22/06/2020.
+//  Copyright © 2020 com.dkcompany.xcodeprojects. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnHover: ViewModifier {
+    @State private var hover = false
+
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    func body(content: Content) -> some View {
+        content
+            .onHover(perform: { _ in
+                self.hover.toggle()
+            })
+            .background(hover ?
+                RoundedCorners(color: Color.white.opacity(0.2), tl: self.tl, tr: self.tr, bl: self.bl, br: self.br) :
+                RoundedCorners(color: Color.clear))
+    }
+}
