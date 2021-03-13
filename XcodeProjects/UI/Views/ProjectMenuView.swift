3ea2a436a2c935723a4b4353379736f5f224d6ed
Kalachniuk, Dima <dkalachniuk@gmail.com>
2021-03-13 11:28:36 +0100

    fixed a bug when podfile lock was removed for a project but the menu still shows the action remove podfile lock

--- XcodeProjects/UI/Views/ProjectMenuView.swift
+++ XcodeProjects/UI/Views/ProjectMenuView.swift
@@ -29,13 +29,17 @@ struct ProjectMenuView: View {
                 TerminalCommandButton(project: project, command: .podUpdate)
                 TerminalCommandButton(project: project, command: .podDeintegrate)
                 if project.hasPodfileLock {
-                    TerminalCommandButton(project: project, command: .removePodfileLock)
+                    TerminalCommandButton(project: project, command: .removePodfileLock) {
+                        preferences.updateProjectMenu = true
+                    }
                 }
                 VStack { Divider() }
             }
 
             if project.hasDerivedData {
-                TerminalCommandButton(project: project, command: .clearProjectDerivedData)
+                TerminalCommandButton(project: project, command: .clearProjectDerivedData) {
+                    preferences.updateProjectMenu = true
+                }
                 VStack { Divider() }
             }
 
